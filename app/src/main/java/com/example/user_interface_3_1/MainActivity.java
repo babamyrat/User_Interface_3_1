9fd61c09e6a19cc8d4b4da14a0936c3eb5c62068
Admin <tmstudentler@gmail.com>
2021-08-25 15:09:20 +0300

    step 4 Listener

--- app/src/main/java/com/example/user_interface_3_1/MainActivity.java
+++ app/src/main/java/com/example/user_interface_3_1/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.user_interface_3_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myStringArray);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(messageClickedHandler);
     }
+    // Create a message handling object as an anonymous class.
+    private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            // Do something in response to the click
+            String selectedItem = (String) parent.getItemAtPosition(position);
+            Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
 }
