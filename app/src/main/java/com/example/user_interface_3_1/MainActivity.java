31edef5f70ffc54912bebfa499c4253bea195a68
Admin <tmstudentler@gmail.com>
2021-08-25 15:21:56 +0300

    step 5 MotionLayout

--- app/src/main/java/com/example/user_interface_3_1/MainActivity.java
+++ app/src/main/java/com/example/user_interface_3_1/MainActivity.java
@@ -14,30 +14,12 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    String[] myStringArray = {"hello", "string", "View", "keyword", "help", "help me!", "help me please!"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myStringArray);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(messageClickedHandler);
     }
-    // Create a message handling object as an anonymous class.
-    private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView parent, View v, int position, long id) {
-            // Do something in response to the click
-            String selectedItem = (String) parent.getItemAtPosition(position);
-            Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
-        }
-    };
-
 
 }
