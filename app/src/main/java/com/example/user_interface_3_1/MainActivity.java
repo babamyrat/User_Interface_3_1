9767c6e7ff8d38b03242e19c6554d62a8e55f45b
Admin <tmstudentler@gmail.com>
2021-08-25 14:38:14 +0300

    step 3 Cursor

--- app/src/main/java/com/example/user_interface_3_1/MainActivity.java
+++ app/src/main/java/com/example/user_interface_3_1/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.user_interface_3_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Data;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    String[] myStringArray = {"hello", "string", "View", "keyword", "help", "help me!", "help me please!"};
+    Cursor cursor; // database cursor
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myStringArray);
+        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+        int[] toViews = {R.id.display_name, R.id.phone_number};
 
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.person_name_and_number, cursor, fromColumns, toViews, 0);
         listView.setAdapter(adapter);
+
     }
 }
