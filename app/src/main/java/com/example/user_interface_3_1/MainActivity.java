f7fc698a8fa31d137a22d267521b20aba4c1f000
Admin <tmstudentler@gmail.com>
2021-08-25 12:56:20 +0300

    step 1 String Array

--- app/src/main/java/com/example/user_interface_3_1/MainActivity.java
+++ app/src/main/java/com/example/user_interface_3_1/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.user_interface_3_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] myStringArray = {"hello", "string", "View", "keyword", "help", "help me!", "help me please!"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myStringArray);
+
+        listView.setAdapter(adapter);
     }
 }
